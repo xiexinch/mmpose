cbe0621703d62f91025ffbd2c8c55cdd38d4187b
xiexinch <xiexinch@outlook.com>
2023-11-24 18:30:36 +0800

    fix

--- mmpose/datasets/datasets/wholebody3d/ubody3d_coco_dataset.py
+++ mmpose/datasets/datasets/wholebody3d/ubody3d_coco_dataset.py
@@ -1,6 +1,8 @@
 # Copyright (c) OpenMMLab. All rights reserved.
+import copy
 
-# from mmpose.codecs.utils import camera_to_image_coord
+import numpy as np
+
 from mmpose.registry import DATASETS
 from mmpose.utils import SimpleCamera
 from ..base import BaseCocoStyleDataset
@@ -12,16 +14,71 @@ class UBody3DCOCODataset(BaseCocoStyleDataset):
     METAINFO: dict = dict(from_file='configs/_base_/datasets/ubody3d.py')
 
     def parse_data_info(self, raw_data_info: dict) -> dict:
-        data_info = super().parse_data_info(raw_data_info)
-        camera_param = data_info['camera_param']
+        ann = raw_data_info['raw_ann_info']
+        img = raw_data_info['img_info']
+
+        if 'bbox' not in ann or 'keypoints' not in ann:
+            return None
+
+        img_w, img_h = img['width'], img['height']
+
+        # get bbox in shape [1, 4], formatted as xywh
+        x, y, w, h = ann['bbox']
+        x1 = np.clip(x, 0, img_w - 1)
+        y1 = np.clip(y, 0, img_h - 1)
+        x2 = np.clip(x + w, 0, img_w - 1)
+        y2 = np.clip(y + h, 0, img_h - 1)
+
+        bbox = np.array([x1, y1, x2, y2], dtype=np.float32).reshape(1, 4)
+
+        keypoints = np.array(ann['keypoints'], dtype=np.float32).reshape(-1, 2)
+        keypoints_visible = np.array(
+            ann['keypoints_valid'], dtype=np.float32).reshape(-1, 1)
+
+        keypoints_3d = np.array(
+            ann['keypoints_3d'], dtype=np.float32).reshape(-1, 3)
+
+        if 'num_keypoints' in ann:
+            num_keypoints = ann['num_keypoints']
+        else:
+            num_keypoints = np.count_nonzero(keypoints.max(axis=2))
+
+        if 'area' in ann:
+            area = np.array(ann['area'], dtype=np.float32)
+        else:
+            area = np.clip((x2 - x1) * (y2 - y1) * 0.53, a_min=1.0, a_max=None)
+            area = np.array(area, dtype=np.float32)
+
+        camera_param = ann['camera_param']
         camera_param = {
             'f': camera_param['focal'],
             'c': camera_param['princpt']
         }
         camera = SimpleCamera(camera_param)
-        keypoints_3d_camera = camera.pixel_to_camera(data_info['keypoints_3d'])
+        keypoints_3d_camera = camera.pixel_to_camera(keypoints_3d)
 
-        data_info['keypoints_3d_cam'] = keypoints_3d_camera
-        data_info['camera_param'] = camera_param
+        data_info = {
+            'img_id': ann['image_id'],
+            'img_path': img['img_path'],
+            'bbox': bbox,
+            'bbox_score': np.ones(1, dtype=np.float32),
+            'num_keypoints': num_keypoints,
+            'keypoints': keypoints,
+            'keypoints_visible': keypoints_visible,
+            'keypoints_3d': keypoints_3d,
+            'keypoints_3d_cam': keypoints_3d_camera,
+            'camera_param': camera_param,
+            'area': area,
+            'iscrowd': ann.get('iscrowd', 0),
+            'segmentation': ann.get('segmentation', None),
+            'id': ann['id'],
+            'category_id': np.array(ann['category_id']),
+            # store the raw annotation of the instance
+            # it is useful for evaluation without providing ann_file
+            'raw_ann_info': copy.deepcopy(ann),
+        }
+
+        if 'crowdIndex' in img:
+            data_info['crowd_index'] = img['crowdIndex']
 
         return data_info
