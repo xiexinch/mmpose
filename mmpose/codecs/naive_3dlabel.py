01747ede6d85506ea122c42b1543710cd5aaa583
xiexinch <xiexinch@outlook.com>
2023-12-03 21:38:55 +0800

    --fix=fix camera transform

--- mmpose/codecs/naive_3dlabel.py
+++ mmpose/codecs/naive_3dlabel.py
@@ -8,14 +8,15 @@ import numpy as np
 from mmpose.registry import KEYPOINT_CODECS
 from mmpose.utils import SimpleCamera
 from .base import BaseKeypointCodec
-
-# from .utils import pixel_to_camera
+from .utils import camera_to_pixel
 
 
 @KEYPOINT_CODECS.register_module()
 class Naive3DLabel(BaseKeypointCodec):
 
-    auxiliary_encode_keys = {'transformed_keypoints', 'keypoints_3d'}
+    auxiliary_encode_keys = {
+        'transformed_keypoints', 'camera_param', 'keypoints_3d'
+    }
 
     label_mapping_table = dict(
         keypoint_x_labels='keypoint_x_labels',
@@ -90,12 +91,17 @@ class Naive3DLabel(BaseKeypointCodec):
                keypoints: np.ndarray,
                keypoints_3d: np.ndarray,
                transformed_keypoints: np.ndarray,
+               camera_param: dict,
                keypoints_visible: Optional[np.ndarray] = None) -> dict:
         """Encode keypoints to 3D labels."""
 
         if not self.test_mode:
+            kpts_3d = transformed_keypoints.copy()
+            fx, fy = camera_param['f']
+            cx, cy = camera_param['c']
+            kpts_3d_image = camera_to_pixel(kpts_3d, fx, fy, cx, cy)
             x, y, z, weights = self._generate_gaussian(
-                transformed_keypoints, keypoints_visible)  # noqa
+                kpts_3d_image, keypoints_visible)  # noqa
             encoded = dict(
                 keypoint_x_labels=x,
                 keypoint_y_labels=y,
@@ -143,7 +149,7 @@ class Naive3DLabel(BaseKeypointCodec):
         if camera_param is None:
             camera_param = self.camera_param
         camera = SimpleCamera(camera_param)
-        keypoints_xy = camera.pixel_to_camera(keypoints_xy)
+        keypoints = camera.pixel_to_camera(keypoints)
         return keypoints, scores
 
     def _map_coordinates(self,
--- mmpose/utils/camera.py
+++ mmpose/utils/camera.py
@@ -161,8 +161,8 @@ class SimpleCamera(SingleCameraBase):
         assert isinstance(X, np.ndarray)
         assert X.ndim >= 2 and X.shape[-1] == 3
         _X = X.copy()
-        _X[:, :2] = (X[:, :2] - self.param['c'].T) / self.param['f'].T * X[:,
-                                                                           [2]]
+        _X[..., :2] = (X[..., :2] -
+                       self.param['c'].T) / self.param['f'].T * X[:, [2]]
         return _X
 
 
