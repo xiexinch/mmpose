b4d41830b215465b57587ff9020eef90654e987d
xiexinch <xiexinch@outlook.com>
2023-12-27 21:54:20 +0800

    add mpii_3dhp

--- mmpose/datasets/datasets/wholebody3d/__init__.py
+++ mmpose/datasets/datasets/wholebody3d/__init__.py
@@ -3,9 +3,10 @@ from .agora_dataset import AgoraDataset
 from .coco_wholebody_3d import COCOWholebody3D
 from .h3wb_dataset import H3WBDataset, H36MWholeBodyDataset
 from .mpii_3d_dataset import MPII3DWBDataset
+from .mpii_3dhp_dataset import MPII3DHPWBDataset
 from .ubody3d_dataset import UBody3dDataset
 
 __all__ = [
     'UBody3dDataset', 'H36MWholeBodyDataset', 'H3WBDataset', 'AgoraDataset',
-    'COCOWholebody3D', 'MPII3DWBDataset'
+    'COCOWholebody3D', 'MPII3DWBDataset', 'MPII3DHPWBDataset'
 ]
