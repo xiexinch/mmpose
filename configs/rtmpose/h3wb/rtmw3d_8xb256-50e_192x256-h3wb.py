9102ac6027e7f0377558200f29b8304721022719
xiexinch <xiexinch@outlook.com>
2023-12-28 14:11:39 +0800

    --fix=fix config

--- configs/rtmpose/h3wb/rtmw3d_8xb256-50e_192x256-h3wb.py
+++ configs/rtmpose/h3wb/rtmw3d_8xb256-50e_192x256-h3wb.py
@@ -95,11 +95,6 @@ model = dict(
         decoder=codec),
     test_cfg=dict(flip_test=True, ))
 
-# base dataset settings
-dataset_type = 'H36MWholeBodyDataset'
-data_mode = 'topdown'
-data_root = 'data/h36m/'
-
 backend_args = dict(backend='local')
 
 # pipelines
@@ -110,7 +105,7 @@ train_pipeline = [
     dict(type='RandomHalfBody'),
     dict(
         type='RandomBBoxTransform', scale_factor=[0.6, 1.4], rotate_factor=80),
-    dict(type='TopdownAffine', input_size=codec['input_size']),
+    dict(type='TopdownAffine3D', input_size=codec['input_size']),
     dict(type='mmdet.YOLOXHSVRandomAug'),
     dict(
         type='Albumentation',
@@ -133,7 +128,7 @@ train_pipeline = [
 val_pipeline = [
     dict(type='LoadImage', backend_args=backend_args),
     dict(type='GetBBoxCenterScale'),
-    dict(type='TopdownAffine', input_size=codec['input_size']),
+    dict(type='TopdownAffine3D', input_size=codec['input_size']),
     dict(type='PackPoseInputs')
 ]
 
@@ -147,7 +142,7 @@ train_pipeline_stage2 = [
         shift_factor=0.,
         scale_factor=[0.75, 1.25],
         rotate_factor=60),
-    dict(type='TopdownAffine', input_size=codec['input_size']),
+    dict(type='TopdownAffine3D', input_size=codec['input_size']),
     dict(type='mmdet.YOLOXHSVRandomAug'),
     dict(
         type='Albumentation',
