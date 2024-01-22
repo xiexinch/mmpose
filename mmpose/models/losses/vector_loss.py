ad4ed697b6d71a7ab178b0c00ff3eec4ee1aff48
xiexinch <xiexinch@outlook.com>
2024-01-22 15:54:00 +0800

    --update=new vs loss

--- mmpose/models/losses/vector_loss.py
+++ mmpose/models/losses/vector_loss.py
@@ -12,12 +12,10 @@ from mmpose.registry import MODELS
 class VectorSimilarityLoss(nn.Module):
 
     def __init__(self,
-                 anchor_indices: List,
                  kpts_indices: List,
                  loss_weight=1.,
                  loss_name: str = 'vs_loss'):
         super().__init__()
-        self.anchor_indices = anchor_indices
         self.kpts_indices = kpts_indices
         self.loss_weight = loss_weight
         self._loss_name = loss_name
@@ -27,31 +25,22 @@ class VectorSimilarityLoss(nn.Module):
                 label: torch.Tensor,
                 target_weights: torch.Tensor = None,
                 **kwargs):
-        anchor_indices = self.anchor_indices
         kpts_indices = self.kpts_indices
 
-        pred_anchors, pred_kpts = pred[:, anchor_indices], pred[:,
-                                                                kpts_indices]
-        label_anchors, label_kpts = label[:,
-                                          anchor_indices], label[:,
-                                                                 kpts_indices]
+        pred_kpts = pred[:, kpts_indices]
+        label_kpts = label[:, kpts_indices]
 
-        num_anchors, num_kpts = len(anchor_indices), len(kpts_indices)
-        pred_anchors = pred_anchors.repeat_interleave(num_kpts, dim=1)
-        pred_kpts = pred_kpts.repeat_interleave(num_anchors, dim=1)
-        label_anchors = label_anchors.repeat_interleave(num_kpts, dim=1)
-        label_kpts = label_kpts.repeat_interleave(num_anchors, dim=1)
+        pred_center = pred_kpts.mean(dim=1, keepdim=True)
+        label_center = label_kpts.mean(dim=1, keepdim=True)
 
-        pred_vecs = pred_kpts - pred_anchors
-        label_vecs = label_kpts - label_anchors
+        pred_vecs = pred_kpts - pred_center
+        label_vecs = label_kpts - label_center
 
         # cosine similarity
         cosine_similarity = F.cosine_similarity(pred_vecs, label_vecs, dim=-1)
         loss = 1 - cosine_similarity
         if target_weights is not None:
             target_weights = target_weights[:, kpts_indices]
-            target_weights = target_weights.repeat_interleave(
-                num_anchors, dim=1)
             loss = loss * target_weights
         loss = loss.mean() * self.loss_weight
         return loss
